aea845a2b560a4d7575be4b5a20cc3f58de15e98
jess-ingraham <jli232@nyu.edu>
2018-11-19 15:07:21 -0500

    database configuration

---
+++ dbconfig.py
@@ -0,0 +1,8 @@
+http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=XXXXXXXXXXXXXXXXXXXXXXX&vanityurl=userVanityUrlName
+
+IP = '35.237.23.200'
+PORT = '3306'
+USER = 'root'
+PASS = '2x2rj0qydKmO42jG'
+
+STEAMKEY = 'B280DA6909B1E40A67ABADB437979FBD'
