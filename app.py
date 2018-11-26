10c0bed1ea822553188027a70e987fcf8d9d5875
jess-ingraham <jli232@nyu.edu>
2018-11-26 14:26:00 -0500

    app started

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
 import dbconfig as config
 
+VanityUrl, steamId, appId = '','',''
+
+steamApiEndpoints = {
+	'getUserID': f'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=B280DA6909B1E40A67ABADB437979FBD&vanityurl={VanityUrl}'
+	,'getUserInfo': f' http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=B280DA6909B1E40A67ABADB437979FBD&steamids={steamId}'
+	,'getFriendsList': f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=B280DA6909B1E40A67ABADB437979FBD&steamid={steamId}&relationship=friend'
+	,'getOwnedGames': f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B280DA6909B1E40A67ABADB437979FBD&steamid={steamId}&format=json'
+	,'getGameSchema': f'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=B280DA6909B1E40A67ABADB437979FBD&appid={appId}'
+}
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -28,6 +39,7 @@ def search_result():
 @app.errorhandler(404)
 def error_page(e):
 	return render_template('notFound.html'), 404
+
 
 app.run(host='0.0.0.0', port=5000, debug=True)
 
